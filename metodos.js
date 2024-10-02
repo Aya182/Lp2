fd6da559cfc40f150520631be5e9b29b747ead65
Aya182 <126289915+Aya182@users.noreply.github.com>
2024-10-02 17:19:11 -0400

    Add files via upload

---
+++ metodos.js
@@ -0,0 +1,22 @@
+//MÉTODO PUSH
+console.log("MÉTODO PUSH");
+let alunos = ["Aya", "Luiz", "Iasmin"];
+//O push adiciona algo na variavel
+alunos.push("Jão Pedro");
+console.log(alunos);
+console.log();
+
+//MÉTODO SHIFT
+console.log("MÉTODO SHIFT");
+let personagemDeAnimes = ["Dio", "Rimuru", "Soma"];
+//O shift Retira a primeira coisa da variavel/Array
+personagemDeAnimes.shift();
+console.log(personagemDeAnimes);
+console.log();
+
+//MÉTODO SORT
+console.log("MÉTODO SORT");
+let jogos = ["Roblox", "Minecreft", "ClashRoyale", "Valorant"];
+//O Sort ordena 
+jogos.sort()
+console.log(jogos);
